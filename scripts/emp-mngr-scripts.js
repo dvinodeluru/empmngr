a64d51e2a8acb53223c1f0d74c36debeccc2b71a
dvinodeluru <dvinod.eluru@gmail.com>
2017-10-30 00:14:27 +0530

    json data binding

--- scripts/emp-mngr-scripts.js
+++ scripts/emp-mngr-scripts.js
@@ -15,7 +15,28 @@ for (let i = 0; i < closeModal.length; i ++) {
         modal.classList.add("off");;
     });
 }
-
+function jsonInit(){
+    let extHttp = new XMLHttpRequest();
+    extHttp.onreadystatechange = function() {  
+           if (this.readyState == 4 && this.status == 200) {    
+                 let restHttp = JSON.parse(this.responseText), $empuser = restHttp.empuser, 
+                  htmlElm = "";    
+                  htmlElm += "<thead><tr><th>First Name</th><th>Last Name</th><th>Gender</th><th>Email</th><th>Phone</th><th>State</th><th>Actions</th></tr></thead>";
+                  htmlElm += "<tbody>";
+                 for (let i = 0; i < $empuser.length; i++) {
+                     if($empuser[i].state == "Active"){
+                        htmlElm += "<tr><td>" + $empuser[i].firstname + "</td><td>" + $empuser[i].lastname + "</td><td>"+ $empuser[i].gender + " </td><td>"+ $empuser[i].email + " </td><td>"+ $empuser[i].phone + " </td><td>"+ $empuser[i].state + "</td><td><div class='action-tool'><button class='btn btn-none viewItem'><i class='fa fa-file-o'></i></button><button class='btn btn-none updateItem'><i class='fa fa-pencil'></i></button><button class='btn btn-none deleteItem'><i class='fa fa-close'></i></button></div></td></tr>";     
+                    }else{
+                        htmlElm += "<tr class='inactive'><td>" + $empuser[i].firstname + "</td><td>" + $empuser[i].lastname + "</td><td>"+ $empuser[i].gender + " </td><td>"+ $empuser[i].email + " </td><td>"+ $empuser[i].phone + " </td><td>"+ $empuser[i].state + "</td><td><div class='action-tool'><button class='btn btn-none viewItem'><i class='fa fa-file-o'></i></button><button class='btn btn-none updateItem'><i class='fa fa-pencil'></i></button><button class='btn btn-none deleteItem'><i class='fa fa-close'></i></button></div></td></tr>";     
+                    }
+                }   
+                  htmlElm += "</tbody>";
+                  document.getElementById('empTable').innerHTML = htmlElm;                   
+          }
+    };
+    extHttp.open("GET", "json/emp-record.json", true);
+    extHttp.send();
+}jsonInit();
 saveItem.addEventListener('click',()=>{
     let firstName = document.getElementById('firstname').value, 
         lastName = document.getElementById('lastname').value 
@@ -23,19 +44,19 @@ saveItem.addEventListener('click',()=>{
         email = document.getElementById('email').value, 
         phone = document.getElementById('phone').value, 
         status = document.getElementById('emp-state').value,
-        tblemp = document.getElementById('empTable');
-        
+        tblemp = document.getElementById('empTable');        
 
-var empuser = JSON.parse(empuser);  //parse the JSON
-empuser.employees.push({        //add the employee
-    firstname:firstName,
-    lastname:lastName,
-    gender:gender,
-    email:email,
-    phone:phone,
-    status:status
+       
+   /* var empuser = JSON.parse(empuser);  //parse the JSON
+    empuser.employees.push({        //add the employee
+        firstname:firstName,
+        lastname:lastName,
+        gender:gender,
+        email:email,
+        phone:phone,
+        status:status
+    });
+    empuser = JSON.stringify(empuser);  //reserialize to JSON*/
 });
-empuser = JSON.stringify(empuser);  //reserialize to JSON
 
 
-});
