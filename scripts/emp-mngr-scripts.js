06f1d786f7af3c33d15b96507a229908fc87453d
dvinodeluru <dvinod.eluru@gmail.com>
2017-10-29 19:27:20 +0530

    emp change

--- scripts/emp-mngr-scripts.js
+++ scripts/emp-mngr-scripts.js
@@ -1,30 +1,4 @@
 
-//EMP JSON COLLECTION
-var empTable = document.getElementById("empTable");
-
-var totalColumns = Object.keys(emp-user[0]).length;
-var columnNames = [];
-columnNames = Object.keys(emp-user[0]);
-
-var extHttp = new XMLHttpRequest();
-extHttp.onreadystatechange = function() {  
-       if (this.readyState == 4 && this.status == 200) {    
-             var restHttp = JSON.parse(this.responseText), allpets = restHttp.groupPets, 
-              htmlElm = "";    
-             for (var i = 0; i < emp-user.length; i++) {
-                    var row = empTable.insertRow(-1); 
-                    columnNames.forEach(function(){
-                        var cell = row.insertCell(-1);
-                        cell.innerHTML = emp-user[i][columnName];
-                    });                        
-              }                 
-      }
-};
-extHttp.error("Connection Failed");
-extHttp.open("GET", "emp-record.json", true);
-extHttp.send();
-
-
 //CRUD ACTIONS
 let addRow, saveItem, viewRow, updateRow, deleteRow, closeModal, modal;
 addRow = document.getElementById('addItem');
@@ -43,11 +17,25 @@ for (let i = 0; i < closeModal.length; i ++) {
 }
 
 saveItem.addEventListener('click',()=>{
-    let firstname = document.getElementById('firstname').value, 
-        lastname = document.getElementById('lastname').value 
+    let firstName = document.getElementById('firstname').value, 
+        lastName = document.getElementById('lastname').value 
         gender = document.getElementById('gender').value, 
         email = document.getElementById('email').value, 
         phone = document.getElementById('phone').value, 
         status = document.getElementById('emp-state').value,
         tblemp = document.getElementById('empTable');
+        
+
+var empuser = JSON.parse(empuser);  //parse the JSON
+empuser.employees.push({        //add the employee
+    firstname:firstName,
+    lastname:lastName,
+    gender:gender,
+    email:email,
+    phone:phone,
+    status:status
+});
+empuser = JSON.stringify(empuser);  //reserialize to JSON
+
+
 });
