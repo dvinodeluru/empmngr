23524963fc0fe76785e8fb1417e7344196266f0a
dvinodeluru <dvinod.eluru@gmail.com>
2017-10-30 15:57:38 +0530

    update script

--- scripts/emp-mngr-scripts.js
+++ scripts/emp-mngr-scripts.js
@@ -10,9 +10,9 @@ function jsonInit(){
                   htmlElm += "<tbody>";
                  for (let i = 0; i < $empuser.length; i++) {
                      if($empuser[i].state == "Active"){
-                        htmlElm += "<tr><td>" + $empuser[i].firstname + "</td><td>" + $empuser[i].lastname + "</td><td>"+ $empuser[i].gender + " </td><td>"+ $empuser[i].email + " </td><td>"+ $empuser[i].phone + " </td><td>"+ $empuser[i].state + "</td><td><div class='action-tool'><button class='btn btn-none viewItem'><i class='fa fa-file-o'></i></button><button class='btn btn-none updateItem'><i class='fa fa-pencil'></i></button><button class='btn btn-none deleteItem'><i class='fa fa-close'></i></button></div></td></tr>";     
+                        htmlElm += "<tr><td>" + $empuser[i].firstname + "</td><td>" + $empuser[i].lastname + "</td><td>"+ $empuser[i].gender + " </td><td>"+ $empuser[i].email + " </td><td>"+ $empuser[i].phone + " </td><td>"+ $empuser[i].state + "</td><td><div class='action-tool'><button class='btn btn-none viewItem'><i class='fa fa-file-o'></i></button><button class='btn btn-none updateItem' onclick='updateRow(this);'><i class='fa fa-pencil'></i></button><button class='btn btn-none deleteItem' onclick='deleteRow(this);'><i class='fa fa-close'></i></button></div></td></tr>";     
                     }else{
-                        htmlElm += "<tr class='inactive'><td>" + $empuser[i].firstname + "</td><td>" + $empuser[i].lastname + "</td><td>"+ $empuser[i].gender + " </td><td>"+ $empuser[i].email + " </td><td>"+ $empuser[i].phone + " </td><td>"+ $empuser[i].state + "</td><td><div class='action-tool'><button class='btn btn-none viewItem'><i class='fa fa-file-o'></i></button><button class='btn btn-none updateItem'><i class='fa fa-pencil'></i></button><button class='btn btn-none deleteItem'><i class='fa fa-close'></i></button></div></td></tr>";     
+                        htmlElm += "<tr class='inactive'><td>" + $empuser[i].firstname + "</td><td>" + $empuser[i].lastname + "</td><td>"+ $empuser[i].gender + " </td><td>"+ $empuser[i].email + " </td><td>"+ $empuser[i].phone + " </td><td>"+ $empuser[i].state + "</td><td><div class='action-tool'><button class='btn btn-none viewItem'><i class='fa fa-file-o'></i></button><button class='btn btn-none updateItem' onclick='updateRow(this);'><i class='fa fa-pencil'></i></button><button class='btn btn-none deleteItem' onclick='deleteRow(this);'><i class='fa fa-close'></i></button></div></td></tr>";     
                     }
                 }   
                   htmlElm += "</tbody>";
@@ -29,48 +29,57 @@ addRow = document.getElementById('addItem');
 modal = document.getElementById("modal");
 saveItem = document.getElementById('saveItem');
 
-closeModal = document.querySelectorAll('.closeDialog');
+//MODAL Dialog Actions
 addRow.addEventListener('click',()=>{
-    modal.classList.remove("off");
-    
-})
+    modal.classList.remove("off");    
+});
+
+closeModal = document.querySelectorAll('.closeDialog');
 for (let i = 0; i < closeModal.length; i ++) {    
     closeModal[i].addEventListener('click',()=>{
         modal.classList.add("off");
     });
 }
 
+var operation = "A"; //"A"=Adding; "E"=Editing
+var selected_index = -1; //Index of the selected list item
+var empresource = localStorage.getItem("empresource");//Retrieve the stored data
+empresource = JSON.parse(empresource); //Converts string to object
+if(empresource == null) //If there is no data, initialize an empty array
+    empresource = [];
 
 saveItem.addEventListener('click',()=>{
-    let firstName = document.getElementById('firstname').value, 
-        lastName = document.getElementById('lastname').value 
-        gender = document.getElementById('gender').value, 
-        email = document.getElementById('email').value, 
-        phone = document.getElementById('phone').value, 
-        status = document.getElementById('emp-state').value,
-        tblemp = document.getElementById('empTable');        
-
-        let xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-        xmlhttp.open("POST", "json/emp-record.json", true);
-        xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xmlhttp.send(JSON.stringify({ firstname:firstName,
-            lastname:lastName,
-            gender:gender,
-            email:email,
-            phone:phone,
-            status:status}));
-            modal.classList.add("off");
-       /* extHttp.open("POST", "json/emp-record.json", true);
-   var empuser = JSON.parse(empuser);  //parse the JSON
-    empuser.push({        //add the employee
-        firstname:firstName,
-        lastname:lastName,
-        gender:gender,
-        email:email,
-        phone:phone,
-        status:status
+    let empItem = JSON.stringify({
+         FirstName : document.getElementById('firstname').value, 
+        LastName : document.getElementById('lastname').value,
+        Gender : document.getElementById('gender').value, 
+        Email : document.getElementById('email').value, 
+        Phone : document.getElementById('phone').value, 
+        Status : document.getElementById('emp-state').value
     });
-    empuser = JSON.stringify(empuser);  //reserialize to JSON*/
+    empresource.push(client);
+	localStorage.setItem("json/emp-record.json", JSON.stringify(empresource));
+	console.log("The data was saved.");	
+    modal.classList.add("off");
+    return true;      
 });
+updateRow = document.querySelectorAll('.updateItem');
 
-
+for (let i = 0; i < updateRow.length; i ++){
+    updateRow.addEventListener('click',()=>{
+        console.log('Update Record');
+    });
+}
+viewRow = document.querySelectorAll('.viewItem');
+for (let i = 0; i < viewRow.length; i ++){
+    viewRow.addEventListener('click', ()=>{
+        console.log('View Record');
+    });
+}
+deleteRow = document.querySelectorAll('.deleteItem');
+for (let i = 0; i < deleteRow.length; i ++){
+// DELETE TABLE ROW.
+    deleteRow.addEventListener('click',()=> {
+        console.log('Delete Record');
+    });
+}
